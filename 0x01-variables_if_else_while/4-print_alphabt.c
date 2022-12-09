0e63e8a424e7fbc3b021bfd204a146e1703e85cc
souadhelmy <soadhamy98@gmail.com>
2022-12-09 08:14:22 -0800

    c1 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry-level
+ * Return: Always 0 (success/correct)
+ */
+
+int main(void)
+{
+
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
