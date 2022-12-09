51eca006fe6ee12acb3527786d5813f1e0e301d3
souadhelmy <soadhamy98@gmail.com>
2022-12-09 08:11:20 -0800

    c1 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char al;															for (al = 'a';  al <= 'z';  al++)
+	{
+		putchar(al);
+	}
+	putchar('\n');
+	return (0);
+}
