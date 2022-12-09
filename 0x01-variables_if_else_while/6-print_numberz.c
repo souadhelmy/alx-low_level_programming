c5bf324973e72321e935bbfda73615ae544eb2d6
souadhelmy <soadhamy98@gmail.com>
2022-12-09 08:17:16 -0800

    c1 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (succed/correct)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
