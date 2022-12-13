023c2129a1f91490072529ba44ad7136e95ed2de
souadhelmy <soadhamy98@gmail.com>
2022-12-13 11:46:41 -0800

    zero nsted

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,25 @@
 #include "main.h"
 
 /**
-*main - function prints text as output
-*
-*Return:return 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
+
 {
-char school[10] = "_putchar";
-int i;
-for (i = 0; i < 8; i++)
-{
-_putchar(school[i]);
-}
-_putchar(10);
-return (0);
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
 }
