4874491e81560f43ac3d8a9c25739b1ad87096c1
souadhelmy <soadhamy98@gmail.com>
2022-12-22 02:24:25 -0800

    6task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,6 @@
  * separators of words are: space, tabulation
  * new line
  * @s: POinter to string
- *
  * Return: Pointer to s
  */
 char *cap_string(char *s)
@@ -14,21 +13,15 @@ char *cap_string(char *s)
 	count = 0;
   while (s[count] != '\0')
 	{
-		if (s[0] >= 97 && s[0] <= 122)
-		{
-			s[0] = s[0] - 32;
-		}
+		if (s[0] >= 97 && s[0] <= 122){
+			s[0] = s[0] - 32;}
 		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
 		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
 		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
 		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
-		    || s[count] == '{' || s[count] == '}')
-		{
-			if (s[count + 1] >= 97 && s[count + 1] <= 122)
-			{
-				s[count + 1] = s[count + 1] - 32;
-			}
-		}
+		    || s[count] == '{' || s[count] == '}'){
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)	{
+				s[count + 1] = s[count + 1] - 32;}}
 		count++;
 	}
 	return (s);
