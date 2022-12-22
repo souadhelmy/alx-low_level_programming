a5f9f8ac08f6bb73243a981f715990ae607032bc
souadhelmy <soadhamy98@gmail.com>
2022-12-22 04:09:38 -0800

    10 task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -17,7 +17,6 @@ int main(void)
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
-	/* ...so that this printsm 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
