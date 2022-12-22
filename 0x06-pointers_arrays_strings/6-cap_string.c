ae821a00db3db2f74705a4d99ea2fcb083244736
souadhelmy <soadhamy98@gmail.com>
2022-12-22 10:04:02 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -35,40 +35,4 @@ char *cap_string(char *str)
                 index++;                                                                                                              
         }                                                                                                                             
                                                                                                                                       
-        return (str);                                                                                                                 
-=======
-#include "main.h"
-
-/**
- * cap_string - capitalizes every first letter of a word in a string
- * separators of words are: space, tabulation
- * new line
- * @s: POinter to string
- * Return: Pointer to s
- */
-char *cap_string(char *s)
-{
-	int count;
-	count = 0;
-  while (s[count] != '\0')
-	{
-		if (s[0] >= 97 && s[0] <= 122)
-		{
-			s[0] = s[0] - 32;
-		}
-		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
-		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
-		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
-		    || s[count] == '{' || s[count] == '}')
-		{
-			if (s[count + 1] >= 97 && s[count + 1] <= 122)
-			{
-				s[count + 1] = s[count + 1] - 32;
-			}
-		}
-		count++;
-	}
-	return (s);
->>>>>>> aa64f3eddc4502770a735e4d7d8e9172f49a6a39
-}
+        return (str);                                                                       }
