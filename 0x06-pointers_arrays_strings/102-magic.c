4689539e1e7fd7c623346061029f38baa313cd92
souadhelmy <soadhamy98@gmail.com>
2022-12-21 18:39:18 -0800

    102task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -22,3 +22,4 @@ int main(void)
         printf("a[2] = %d\n", a[2]);                                                                                                  
         return (0);                                                                                                                   
 }
+}
