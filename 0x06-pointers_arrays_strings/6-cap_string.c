2756f8b71862cc05ecd55b2441351a478696e5cc
souadhelmy <soadhamy98@gmail.com>
2022-12-21 17:11:35 -0800

    6task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,7 @@
  * separators of words are: space, tabulation
  * new line
  * @s: POinter to string
- * Return: Pointer to 0
+ * Return: Pointer to s
  */
 char *cap_string(char *s)
 {
@@ -30,5 +30,5 @@ char *cap_string(char *s)
 		}
 		count++;
 	}
-	return (0);
+	return (s);
 }
