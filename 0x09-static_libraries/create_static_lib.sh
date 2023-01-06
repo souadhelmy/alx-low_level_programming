4fdb0f96b0e7d607f1aa25b08e1652d8ff333497
souadhelmy <soadhamy98@gmail.com>
2023-01-06 19:50:07 +0200

    all

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-<<<<<<< HEAD
 LIBNAME=liball.a
 gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c;
 ar -rc $LIBNAME *.o;
 ranlib $LIBNAME
 rm *.o
-=======
-gcc -Wall -pedantic -Werror -Wextra -c *.c
->>>>>>> 38cdb6c7f7bc273ff7318036fb437d85a66be41a
