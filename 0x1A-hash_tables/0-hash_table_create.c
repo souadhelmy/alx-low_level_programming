2e9e96f3951304f1fed0626834ff5593e6c16bba
souadhelmy <118821486+souadhelmy@users.noreply.github.com>
2023-04-08 01:48:49 +0200

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table.
- * @size: the size of the array.
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
  *
- * Return: If an error occurs return - NULL.
- * 	   Otherwise - a pointer to the new hash table.
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
