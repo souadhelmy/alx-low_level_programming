e99f1856c4358420f53c761203105cb3f3051f61
souadhelmy <118821486+souadhelmy@users.noreply.github.com>
2023-04-08 01:36:19 +0200

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,14 +13,14 @@ hash_table_t *hash_table_create(unsigned long int size);
 	unsigned long int i;
 
 	ht  = malloc (sizeof(hash_table_t));
-	if (ht==NULL);
+	if (ht == NULL);
 	    return (NULL);
 
 	ht->size = size;
 	ht->array = malloc(size*(sizeof(hash_node_t*));
 			if(ht->array==NULL)
 			return (NULL);
-			for (i=1 ; i<= size;i++)
+			for (i=0 ; i < size;i++)
 			ht->array[i]=NULL;
 
 		return (ht);
