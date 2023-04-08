bf995105f80afcf3ad0515fbfa9d4a75b707614e
souadhelmy <soadhamy98@gmail.com>
2023-04-07 17:47:29 -0700

    creation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,21 +7,21 @@
  * Return: If an error occurs - NULL.
  *         Otherwise - a pointer to the new hash table.
  */
-hash_table_t *hash_table_create(unsigned long int size)
+hash_table_t *ht(unsigned long int size)
 {
-	hash_table_t *hash_table_created;
+	hash_table_t *ht;
 	unsigned long int i;
 
-	hash_table_created = malloc(sizeof(hash_table_t));
-	if (hash_table_created == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	hash_table_created->size = size;
-	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
-	if (hash_table_created->array == NULL)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
 	for (i = 1; i <= size; i++)
-	hash_table_created->array[i] = NULL;
+	ht->array[i] = NULL;
 
-	return (hash_table_created);
+	return (ht);
 }
