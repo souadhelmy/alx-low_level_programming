2e1a262c5eb030aa2a964bfa334dc96474c29644
souadhelmy <118821486+souadhelmy@users.noreply.github.com>
2023-04-08 02:42:40 +0200

    error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table_created->array == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= size; i++)
 	hash_table_created->array[i] = NULL;
 
 	return (hash_table_created);
