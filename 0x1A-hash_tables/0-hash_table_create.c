683bdddf0b7af98ebca6e3d5a0566e6acd82c614
souadhelmy <soadhamy98@gmail.com>
2023-04-07 17:52:03 -0700

    creation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	for (i = 1; i <= size; i++)
 		ht->array[i] = NULL;
 
 	return (ht);
